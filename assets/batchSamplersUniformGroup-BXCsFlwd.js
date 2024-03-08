9f0f836bf6e3970e77c37266c33076de204335dd
@v.korniienko <v.korniienko@turbogames.io>
2024-03-08 17:22:18 +0200

    Updates

---
+++ assets/batchSamplersUniformGroup-BXCsFlwd.js
@@ -0,0 +1,1 @@
+import{M as e}from"./index-DGUK4dXh.js";import{U as o}from"./colorToUniform-DG9E8F-u.js";const t=new Int32Array(e);for(let r=0;r<e;r++)t[r]=r;const i=new o({uTextures:{value:t,type:"i32",size:e}},{isStatic:!0});export{i as b};
